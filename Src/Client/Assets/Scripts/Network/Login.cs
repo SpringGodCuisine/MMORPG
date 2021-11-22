9a22a83c0c64df3f33c2a40657c1f764ab10c404
HCCCCC <837647312@qq.com>
2021-11-22 20:17:33 +0800

    版本0.1.1122

---
+++ Src/Client/Assets/Scripts/Network/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Network;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NetClient.Instance.Init("127.0.0.1", 8000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
